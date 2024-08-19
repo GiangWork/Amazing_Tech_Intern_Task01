3d6ec6dccf0d8312ea66fa827a04ec1dd87ab615
Giang <giangdoingwork@gmail.com>
2024-08-19 11:03:31 +0700

    [CRUD] Order and User

---
+++ XuongMay.Contract.Services/Interface/IOrderTaskRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XuongMay.Contract.Repositories.Entity;
+
+namespace XuongMay.Contract.Services.Interface
+{
+    public interface IOrderTaskRepository
+    {
+        Task<OrderTask> AddAsync(OrderTask orderTask);
+        Task<IEnumerable<OrderTask>> GetAllAsync();
+        Task<OrderTask?> GetByIdAsync(string id);
+        Task<OrderTask?> UpdateAsync(OrderTask orderTask);
+        Task<bool> DeleteByIdAsync(string id);
+    }
+}
