363f60b9b514f9f200238f59ac317d22784c50b9
thankiemsama <vinhluan171@gmail.com>
2024-08-19 10:00:46 +0700

    Revert "[CRUD] ORDER and [NOT USER YET]"
    This reverts commit ceb6e112c0cfdb23b2888f09e0673ea20912eec1.

--- XuongMayBE.API/Controllers/OrderTaskController.cs
+++ XuongMayBE.API/Controllers/OrderTaskController.cs
@@ -1,6 +1,4 @@
-﻿using AutoMapper;
-using Microsoft.AspNetCore.Mvc;
-using XuongMay.Contract.Repositories.Entity;
+﻿using Microsoft.AspNetCore.Mvc;
 using XuongMay.Contract.Services.Interface;
 using XuongMay.ModelViews.OrderTaskModelView;
 
@@ -11,50 +9,46 @@ namespace XuongMayBE.API.Controllers
     public class OrderTaskController : ControllerBase
     {
         private readonly IOrderTaskService _orderTaskService;
-        private readonly IMapper _mapper;
 
-        public OrderTaskController(IOrderTaskService orderTaskService, IMapper mapper)
+        public OrderTaskController(IOrderTaskService orderTaskService)
         {
             _orderTaskService = orderTaskService;
-            _mapper = mapper;
         }
 
         [HttpPost("create_OrderTask")]
         public async Task<IActionResult> CreateOrderTask([FromBody] OrderTaskModelView request)
         {
-            var orderTask = _mapper.Map<OrderTask>(request);
-            var result = await _orderTaskService.CreateOrderTask(orderTask);
-            return Ok(_mapper.Map<OrderTaskModelView>(result));
+            var OrderTask = await _orderTaskService.CreateOrderTask(request);
+            return Ok(OrderTask);
         }
 
         [HttpGet("get_AllOrderTasks")]
         public async Task<IActionResult> GetAllOrderTasks()
         {
-            var orderTasks = await _orderTaskService.GetAllOrderTasks();
-            return Ok(_mapper.Map<IEnumerable<OrderTaskModelView>>(orderTasks));
+            var OrderTasks = await _orderTaskService.GetAllOrderTasks();
+            return Ok(OrderTasks);
         }
 
         [HttpGet("get_OrderTaskById/{id}")]
         public async Task<IActionResult> GetOrderTaskById(string id)
         {
-            var orderTask = await _orderTaskService.GetOrderTaskById(id);
-            if (orderTask == null)
+            var OrderTask = await _orderTaskService.GetOrderTaskById(id);
+            if (OrderTask == null)
             {
                 return NotFound();
             }
-            return Ok(_mapper.Map<OrderTaskModelView>(orderTask));
+            return Ok(OrderTask);
         }
 
         [HttpPut("update_OrderTask/{id}")]
         public async Task<IActionResult> UpdateOrderTask(string id, [FromBody] OrderTaskModelView request)
         {
-            var orderTask = _mapper.Map<OrderTask>(request);
-            var result = await _orderTaskService.UpdateOrderTask(id, orderTask);
-            if (result == null)
+            var OrderTask = await _orderTaskService.UpdateOrderTask(id, request);
+            if (OrderTask == null)
             {
                 return NotFound();
             }
-            return Ok(_mapper.Map<OrderTaskModelView>(result));
+            return Ok(OrderTask);
         }
 
         [HttpDelete("delete_OrderTask/{id}")]
